f0388eb3226d62e2b59c7dfce9399ec75209cfbe
Sav <maidenfan78@gmail.com>
2025-09-29 07:50:52 +1000

    scraper update2

--- scripts/testScraper.ts
+++ scripts/testScraper.ts
@@ -1,7 +1,7 @@
-import { fetchBingLinks } from ../server/utils/scraper.js;
+import { fetchBingLinks } from '../server/utils/scraper.js';
 
 async function main() {
- for (const query of [Item 93549004, 93549004 barcode, 93549004 product, 93549004]) {
+ for (const query of ['Item 93549004', '93549004 barcode', '93549004 product', '93549004']) {
  const links = await fetchBingLinks(query, 10);
  console.log(query, links);
  }
